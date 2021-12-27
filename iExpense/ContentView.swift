49b679b2a930f60b37ec8eefd7163669e9120924
citixenken <kmuyesu@gmail.com>
2021-12-27 22:16:34 +0300

    deleting items using onDelete

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,30 +8,38 @@
 import SwiftUI
 
 //allows data to be shared across multiple views
-class User: ObservableObject {
-    @Published var firstName = "eliud"
-    @Published var lastName = "kipchoge"
-}
+//class User: ObservableObject {
+//    @Published var firstName = "eliud"
+//    @Published var lastName = "kipchoge"
+//}
 
-struct secondView: View {
-    
-    @Environment(\.dismiss) var dismiss
-    
-    let name: String
-    
-    var body: some View {
-        Text("Hello, \(name)!")
-        
-        Button("Dismiss") {
-            dismiss()
-        }
-    }
-}
+//struct secondView: View {
+//
+//    @Environment(\.dismiss) var dismiss
+//
+//    let name: String
+//
+//    var body: some View {
+//        Text("Hello, \(name)!")
+//
+//        Button("Dismiss") {
+//            dismiss()
+//        }
+//    }
+//}
 
 struct ContentView: View {
     
-    @State private var showingSheet = false
+//    @State private var showingSheet = false
     //@StateObject var user = User()
+    
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 0
+    
+    //IndexSet tells us the positions of all items in the ForEach that should be removed
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
+    }
     
     var body: some View {
 //        VStack {
@@ -41,11 +49,30 @@ struct ContentView: View {
 //
 //        }
         
-        Button ("Show sheet"){
-            showingSheet.toggle()
-        }
-        .sheet(isPresented: $showingSheet) {
-            secondView(name: "@citixenken")
+//        Button ("Show sheet"){
+//            showingSheet.toggle()
+//        }
+//        .sheet(isPresented: $showingSheet) {
+//            secondView(name: "@citixenken")
+//        }
+        
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                
+                Button ("Increment row number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }
+            .toolbar {
+                EditButton()
+            }
         }
     }
 }
